052449034c05c42470bf818cb8d766584bd5bf94
YOUR NAME <YOUR@EMAIL.com>
2021-01-29 14:17:16 -0500

    just practice

--- src/App.js
+++ src/App.js
@@ -1,25 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState} from 'react';
+import Tweet from './tweet'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+function App(){
+
+  const [isRed, setRed] = useState(false)
+  const [count, setCount] = useState(0)
+
+  const [user, setUser] = useState({
+    name: 'Ed',
+    age: 25,
+    posts: ['my first post', 'my lovely summer']
+  })
+
+  const increment = () => {
+    setCount(count + 1);
+    setRed(!isRed);
+  }
+
+  return(
+    <div className='app'>
+      <h1 className={isRed ? 'red' : ''}>Change my color</h1>
+      <button onClick={increment}>Increment</button>
+      <h1>{count}</h1>
+
     </div>
-  );
+  )
 }
 
 export default App;
